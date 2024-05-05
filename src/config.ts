58f4b23415ba6ac4a66f4eeb90f9616f7f306722
DenizKose <deniz8011@gmail.com>
2024-05-05 22:14:43 +0300

    Update astro

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 export const MODULE_LINK_DFT = 'https://github.com/deniztadice/dft/releases/latest/download/module.json'
+export const MODULE_LINK_DFT_RELEASES = 'https://github.com/deniztadice/dft/releases'
+export const MODULE_LINK_DFT_CHANGELOG = 'https://github.com/deniztadice/dft/blob/main/CHANGELOG.md'
 export const NAVBAR_MENU = [
     ["pages.index", '/'],
     ["pages.index_modules", '/modules', [["pages.index_modules_dft_short", "/modules/dft"]]],
