0dbf252c3fb7289315b49d9436fba87a2eb4ebc3
DenizKose <deniz8011@gmail.com>
2024-05-09 18:48:41 +0300

    Added Жменька квітів 💐

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,14 @@
+// Modules
+
 export const MODULE_LINK_DFT = 'https://github.com/deniztadice/dft/releases/latest/download/module.json'
 export const MODULE_LINK_DFT_RELEASES = 'https://github.com/deniztadice/dft/releases'
 export const MODULE_LINK_DFT_CHANGELOG = 'https://github.com/deniztadice/dft/blob/main/CHANGELOG.md'
+
+// Advenures
+export const ADVENTURE_LINK_AFOF = 'https://github.com/deniztadice/dtd-a-fistful-of-flowers/releases/latest/download/module.json'
+
 export const NAVBAR_MENU = [
     ["pages.index", '/'],
     ["pages.index_modules", '/modules', [["pages.index_modules_dft_short", "/modules/dft"]]],
+    ["pages.index_adventures", '/adventures', [["systems.pf2e_short", "/adventures/pf2e"]]],
 ]
